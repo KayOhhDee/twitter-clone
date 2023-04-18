c3e33ec75d67de3502fefaec002d0bca69184225
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-04-18 23:58:23 +0000

    add right side nav cards and some items it should contain

--- app.vue
+++ app.vue
@@ -5,9 +5,19 @@
       <div class="min-h-full">
         <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
   
-          <div class="md:block xs-col-span-1 xl:col-span-2">
+          <div class="hidden md:block xs-col-span-1 xl:col-span-2">
             <div class="sticky top-0">
               <SideNavLeft />
+            </div>
+          </div>
+
+          <main class="col-span-12 md:col-span-8 xl:col-span-6">
+
+          </main>
+
+          <div class="hidden md:block xl:col-span-4 md:col-span-3">
+            <div class="sticky top-0">
+              <SideNavRight />
             </div>
           </div>
   
---
+++ components/SideNav/Right/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="flex flex-col">
+    <SideNavRightPreviewCard title="What's happening?">
+      <SideNavRightPreviewCardItem v-for="(whatsHappening, index) in whatsHappeningItems" :key="index">
+        <h2 class="font-bold text-gray-800 text-md dark:text-white">{{ whatsHappening.title }}</h2>
+        <p class="text-xs text-gray-400">{{ whatsHappening.count }}</p>
+      </SideNavRightPreviewCardItem>
+    </SideNavRightPreviewCard>
+    <SideNavRightPreviewCard title="Who to follow">
+      <SideNavRightPreviewCardItem v-for="(whoToFollow, index) in whoToFollowItems" :key="index">
+        <div class="flex flex-row justify-between p-2">
+          <div class="flex flex-row">
+            <img class="w-10 h-10 rounded-full" :src="whoToFollow.image" :alt="whoToFollow.name" />
+            <div class="flex flex-col ml-2">
+              <h1 class="text-sm font-bold text-gray-900 dark:text-white">{{ whoToFollow.name }}</h1>
+              <p class="text-xs text-gray-400">{{ whoToFollow.handle }}</p>
+            </div>
+          </div>
+        </div>
+      </SideNavRightPreviewCardItem>
+    </SideNavRightPreviewCard>
+  </div>
+</template>
+
+<script setup>
+const whatsHappeningItems = ref([
+  {
+    title: 'SpaceX',
+    count: '18.8k Tweets'
+  },
+  {
+    title: '#CodingIsFun',
+    count: '8.8k Tweets'
+  },
+  {
+    title: '#artforall',
+    count: '1.8k Tweets'
+  }
+])
+
+const whoToFollowItems = ref([
+  {
+    name: 'Joe Biden', 
+    handle: '@JoeBiden',
+    image: 'https://picsum.photos/200/200'
+  }
+])
+</script>
