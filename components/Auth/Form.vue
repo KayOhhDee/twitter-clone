5ffbf8c3f7bea09a43ede1c65d3920678dbf26d2
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-04-24 22:46:02 +0000

    add auth UI

---
+++ components/Auth/Form.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="pt-5 space-y-6">
+      <Input v-model="credentials.username" label="Username" placeholder="@nickname"/>
+      <Input v-model="credentials.password" label="Password" placeholder="*********" type="password"/>
+    </div>
+  </div>
+</template>
+
+<script setup>
+const credentials = reactive({
+  username: '',
+  password: ''
+})
+</script>
