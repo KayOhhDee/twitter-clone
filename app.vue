4617ea5943e24b5ff3bbb601a653aa33e416f3d1
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-04-30 22:39:32 +0000

    conditionally render app entry view

--- app.vue
+++ app.vue
@@ -1,8 +1,11 @@
 <template>
   <div :class="{'dark': darkMode}">
     <div class="bg-white dark:bg-dim-900">
+      <div v-if="isAuthLoading">
+        <LoadingPage />
+      </div>
 
-      <div class="min-h-full">
+      <div v-else-if="user" class="min-h-full">
         <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
   
           <div class="hidden md:block xs-col-span-1 xl:col-span-2">
@@ -24,10 +27,19 @@
         </div>
       </div>
 
+      <AuthPage v-else />
     </div>
   </div>
 </template>
-
+ 
 <script setup>
 const darkMode = ref(false)
+const { useAuthUser, initAuth, useAuthLoading } = useAuth()
+
+const user = useAuthUser()
+const isAuthLoading = useAuthLoading()
+
+onBeforeMount(() => {
+  initAuth()
+})
 </script>
