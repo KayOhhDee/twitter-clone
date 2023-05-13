e818dd4cfd796eb89c53ffd62e59469b40c95ad9
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-13 23:25:51 +0000

    add tweets backend

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,9 @@ export default defineNuxtConfig({
   runtimeConfig: {
     jwtAccessSecret: process.env.ACCESS_TOKEN_SECRET_JWT,
     jwtRefreshSecret: process.env.REFRESH_TOKEN_SECRET_JWT,
+    
+    cloudinaryCloudName: process.env.CLOUDINARY_CLOUD_NAME,
+    cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+    cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET
   }
 })
