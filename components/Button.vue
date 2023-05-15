228760ce0e2bcf8b0bf76268f2bac2cd0f6fe333
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-15 00:15:15 +0000

    emit click event on button component click

--- components/Button.vue
+++ components/Button.vue
@@ -3,6 +3,7 @@
     class="flex justify-center text-white bg-blue-400 rounded-full hover:bg-blue-500 font-sm disabled:bg-blue-300 disabled:cursor-not-allowed" 
     :disabled="props.disabled"
     :class="classes"
+    @click="handleClick"
   >
     <span :class="textFontSize">
       <slot />
@@ -11,6 +12,8 @@
 </template>
 
 <script setup>
+const emits = defineEmits(['onClick'])
+
 const props = defineProps({
   disabled: {
     type: Boolean,
@@ -46,5 +49,9 @@ const textFontSize = computed(() => {
   }
 })
 
-const classes = computed(() => `${paddingClasses.value}`)
+const classes = computed(() => `${paddingClasses.value} ${props.liquid ? 'w-full' : 'w-min'}`)
+
+const handleClick = (event) => {
+  emits('onClick', event)
+}
 </script>
