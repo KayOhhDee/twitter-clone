f8c7bad260c1f11edd3d62ede1d78988be5a3ce3
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-24 23:55:45 +0000

    render tweet list (partially done)

--- components/Tweet/ListFeed.vue
+++ components/Tweet/ListFeed.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
     <h1>List feed</h1>
+
+    <ul>
+      <li v-for="(tweet, index) in props.tweets" :key="index">{{ tweet }}</li>
+    </ul>
   </div>
 </template>
 
 <script setup>
-
+const props = defineProps({
+  tweets: {
+    type: Array,
+    required: true
+  }
+})
 </script>
