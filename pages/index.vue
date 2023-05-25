6cd96e0aed52c371c753ae698c9b2ed8e9823da7
Kwasi Owusu-Donkor <kod3180@gmail.com>
2023-05-25 22:09:54 +0000

    add Tweet list feed to page

--- pages/index.vue
+++ pages/index.vue
@@ -7,15 +7,29 @@
       <TweetForm :user="user"/>
     </div>
 
-    <TweetListFeed />
+    <TweetListFeed :tweets="homeTweets" />
   </MainContainer>
 </template>
 
 <script setup>
 const { defaultBorderColor } = useTailwind()
+const { getHomeTweets } = useTweets()
 
 const loading = ref(false)
+const homeTweets = ref([])
 
 const { useAuthUser } = useAuth()
 const user = useAuthUser()
+
+onBeforeMount(async () => {
+  loading.value = true
+  try {
+    const {tweets} = await getHomeTweets()
+    homeTweets.value = tweets
+  } catch (error) {
+    console.error(error)
+  } finally {
+    loading.value = false
+  }
+})
 </script>
